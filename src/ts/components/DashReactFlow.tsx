9487a52b6c6bff5225153067b4d62be49a2d809c
Albin Lindqvist <albin.lindqvist@hotmail.se>
2025-10-09 11:06:20 +0200

    initial

---
+++ src/ts/components/DashReactFlow.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {DashComponentProps} from '../props';
+
+type Props = {
+    // Insert props
+} & DashComponentProps;
+
+/**
+ * Component description
+ */
+const DashReactFlow = (props: Props) => {
+    const { id } = props;
+    return (
+        <div id={id}>
+            {/* Insert code */}
+        </div>
+    )
+}
+
+export default DashReactFlow;
---
+++ usage.py
@@ -0,0 +1,10 @@
+import dash_react_flow
+import dash
+
+app = dash.Dash()
+
+app.layout = dash_react_flow.DashReactFlow(id='component')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
